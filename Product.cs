104fa26a2df08298418bc29a4b75255642cc1f11
Vaoth <miguel_molina2@hotmail.es>
2023-09-27 09:20:32 +0200

    Agregar archivos de proyecto.

---
+++ Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasket
+{
+    internal class Product
+    {
+        public uint Id {  get; set; }
+        public string Name { get; set; }
+        public uint Weight { get; set; }
+
+        public Product(uint id, string name, uint weight) {
+            Id = id;
+            Name = name;
+            Weight = weight;
+        }
+
+        public Product() { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ShoppingBasket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = createListFromFile("list.txt");
+
+            uint limitWeight = 20;
+            List<List<Product>> baskets = calculateBaskets(
+                products.OrderByDescending(p => p.Weight).ToList(),
+                limitWeight
+                );
+
+            Console.WriteLine($"With our list of {products.Count} products, we would need {baskets.Count} trips to the super market:\n");
+            for (int i = 0; i < baskets.Count; i++) {
+                Console.WriteLine($"Basket #{i}: {String.Concat(baskets[i].Select(p => $"{p.Name} ({p.Weight} kg) "))}");
+                Console.WriteLine($"TOTAL: {baskets[i].Sum(p => p.Weight)} kg");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        static List<Product> createListFromFile(string filename) {
+            List<Product> products = new List<Product>();
+            string[] lines = File.ReadAllLines(filename);
+            for (uint i = 0; i < lines.Length; i++) {
+                string[] args = lines[i].Split(',');
+
+                if (args.Length != 2) 
+                    continue;
+
+                uint weight;
+                if (!uint.TryParse(args[1], out weight)) 
+                    continue;
+
+                Product product = new Product(i, args[0], weight);
+                products.Add(product);
+            }
+            return products;
+        }
+
+        static List<List<Product>> calculateBaskets(List<Product> products, uint limitWeight) {
+            List<List<Product>> baskets = new List<List<Product>>();
+            while (products.Count > 0)
+            {
+                int i = 0;
+                uint totalWeight = 0;
+                List<Product> currentBasket = new List<Product>();
+
+                while (totalWeight <= limitWeight && i < products.Count)
+                {
+                    Product product = products[i++];
+                    if (totalWeight + product.Weight <= limitWeight)
+                    {
+                        totalWeight += product.Weight;
+                        currentBasket.Add(product);
+                        products.Remove(product);
+                        i = 0;
+                    }
+                }
+                baskets.Add(currentBasket);
+            }
+            return baskets;
+        }
+    }
+}
